0528c1d1033ee9b2ff47499054dbd6581b7e3390
KurangKering <cybercature@gmail.com>
2017-12-26 19:10:39 +0700

    first commit

---
+++ application/libraries/User.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User {
+
+	public function user()
+	{
+
+	}
+	public function login()
+	{
+
+	}
+	public function set_session()
+	{
+
+	}
+	public function ubah_data()
+	{
+
+	}
+
+	public function cek_user()
+	{
+
+	}
+	public function logout()
+	{
+
+	}
+}
